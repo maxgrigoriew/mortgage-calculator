18cb853ab063b2225751e4d35bc34534181b9f7f
Max <sss123123111@yandex.ru>
2022-11-14 12:42:32 +0300

    added vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './assets/scss/main.scss'
+// import './assets/scss/main.scss'
+import vuetify from './plugins/vuetify'
+import isSelect from '@/components/isSelect';
+import isCheckbox from '@/components/isCheckbox';
+
+Vue.component('is-select', isSelect)
+Vue.component('is-checkbox', isCheckbox)
 
 Vue.config.productionTip = false
-    new Vue({
-  render: h => h(App),
+new Vue({
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
